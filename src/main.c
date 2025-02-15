d699b71463e3d51de61affa972f2880a9940e420
Patrik <pat.deloughry@gmail.com>
2025-02-15 08:35:49 -0700

    Some test code updates

--- src/main.c
+++ src/main.c
@@ -1,42 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define ENABLE_DECOMP
+#define DECOMP_ON 1
 #include "cpu.h"
 #include "cpu_instr_set.h"
 #include "mem.h"
+
+#include "boot_rom.h"
 
 int main()
 {
     struct gbc gameboy = {};
     gameboy.mem = mem_create();
+    mem_load_rom(gameboy.mem, boot_rom);
 
-    
-    gameboy.cpu.HL.byte.H = 0x3c;
-    gameboy.cpu.HL.byte.L = 0xbf;
+    bool running = true;
+    while (running)
+    {
+        cpu_exe(&gameboy);
+        if (gameboy.cpu.BC.byte.B == 0x66) running = false;
+    }
 
-    printf("The low byte is: 0x%x\n", gameboy.cpu.HL.byte.L);
-    printf("The high byte is: 0x%x\n", gameboy.cpu.HL.byte.H);
-    printf("The register is: 0x%x\n", gameboy.cpu.HL.val);
-
-    cpu_ld_r16_imm16(&gameboy, R16_DE, 12345);
-
-    printf("The value of DE is: %u\n", gameboy.cpu.DE.val);
-
-    gameboy.mem[12345] = 5;
-    cpu_ld_a_r16mem(&gameboy, R16_DE);
-
-    printf("The value of A is %u \n", gameboy.cpu.AF.byte.A);
-
-    uint8_t daa_num = 0xB5;
-    cpu_ld_r8_imm8(&gameboy, R8_A, daa_num);
-    cpu_daa(&gameboy);
-    printf("The daa value of 0x%x is 0x%x \n", daa_num, gameboy.cpu.AF.byte.A);
-    printf("C = %u\n", gameboy.cpu.AF.flag.C);
-
-    uint8_t test = 0x8F;
-    int8_t test2 = test;
-    printf("The unsigned is %u and the signed is %i\n", test, test2);
+    printf("CPU stopped on B = 0x66\n");
 
     mem_destroy(gameboy.mem);
 
